da372a5df0d34d7e1c361fe67791ae470daabb1d
Fatima Abair <fatima-azahara.abair-naimi@etu.umontpellier.fr>
2025-07-23 14:52:17 +0200

    feat: add version prop to QuestionnaireResume component

--- front-server/src/components/Questionnaire/questionnaireResume.jsx
+++ front-server/src/components/Questionnaire/questionnaireResume.jsx
@@ -1,12 +1,15 @@
-function QuestionnaireResume( { idQuestionnaire, label } ) {
+function QuestionnaireResume( { idQuestionnaire, label, version } ) {
     return (
         <div className="questionnaire-resume">
-            <h2>{label}</h2>
+            <label>Edition : {lable}</label>
+            <Search className = "icone" />
+            <input type="text" placeholder="Rechercher" />
+            <input type="text"placeholder="Nom questionnaire" />
+            <input type="text" placeholder="Description"/>
+            <p>Version {version} - Nombre de pages : N</p>
             <p>{idQuestionnaire}</p>
         </div>
     );
 }
-
-
 
 export default QuestionnaireResume;
