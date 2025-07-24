2e91173c4a67527de120b6f8748a67510536d687
Arthur-Henri Michalland <ah.michalland@gmail.com>
2025-07-24 15:55:59 +0200

    modifications on create questionnaire to post questionnaire infos to the back; and edit section to get section infos and post updated infos to the back, need to create back logic

--- front-server/src/components/popups/editSection.jsx
+++ front-server/src/components/popups/editSection.jsx
@@ -1,16 +1,79 @@
 import { saveButton } from '../button/button';
 
-function PopUpEditSection({id, trigger, setTrigger}) {
+function PopUpEditSection({idSection, trigger, setTrigger}) {
     // Logic to display a popup for editing a section
-    console.log("Editing section with id:", id);
+    console.log("Editing section with id:", idSection);
+
+    const [formData, setFormData] = useState({
+    label:'',
+    description:'',
+    position:'',
+    tooltip:'',
+    scoremax:''
+  });
+
+    async function fetchSectionInfo(idSection) {
+    try {
+      const response = await fetch(`/getSection/${idSection}`)
+
+      const data = await response.json();
+      console.log('Success:', data);
+      return data;
+    } catch (error) {
+      console.error('Failed to retrieve section infos: ', error);
+      const errData = await response.json();
+      console.error('Error:', errData.message);
+
+        // display an error popup to inform that the section infos could not be fetched
+
+      return;
+    }
+  };
+
+  function handleChange(element) {
+    const { name, value } = element.target;
+    const cleanValue = name === 'scoreMax' ? value.replace(/[^0-9]/g, '') : value;
+    setFormData((dataset) => ({
+      ...dataset,
+      [name]: cleanValue,
+    }));
+  };
+
+  async function postSection(idSection) {
+    try {
+      const response = await fetch(`/updateSection/${idSection}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await response.json();
+      console.log('Success:', data);
+      setTrigger(false);
+    } catch (error) {
+      console.error('Fetch failed', error);
+      const errData = await response.json();
+      console.error('Error:', errData.message);
+
+        // display an error popup to inform that the section could not be updated
+
+      return;
+    }
+  };
+
+  const {label, description, position, tooltip, scoreMax} = fetchSectionInfo(idSection);
+
     return (trigger)?  (
         <div className="popup">
             <div className="popup-inner">
                 <h2>Modification section</h2>
-                <input type="text" placeholder="Intitulé"/>
-                <input type="text" placeholder="Aide"/>
-                <p>Description</p>
-                <textarea className=""/>
+                <input type="text" placeholder={label}/>
+                <textarea placeholder={description}></textarea>
+                <input type="number" placeholder={position}/>
+                <textarea placeholder={tooltip}> </textarea>
+                <input type="number" placeholder={scoreMax} name="scoreMax" step="1" min="0" value={formData.scoreMax} onChange={handleChange} />
                 {saveButton(id)}
                 <button className="red-btn" onClick={() => setTrigger(false)}>Annuler</button>
             </div>
@@ -19,4 +82,3 @@ function PopUpEditSection({id, trigger, setTrigger}) {
 }
 
 export default PopUpEditSection;
-
